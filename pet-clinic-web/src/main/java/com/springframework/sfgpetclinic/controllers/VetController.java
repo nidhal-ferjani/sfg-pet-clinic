5701bb39091bfcd912003ec4731b3bdcc551e56f
Nidhal Ferjani <nidhal.ferjani@gmail.com>
2019-06-02 16:45:39 +0100

    added vets List and updated Controller. closes #12

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.springframework.sfgpetclinic.controllers;
 
+import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index" ,"/vets/index.html"})
-    public  String listVets(){
-       return "vets/index";
+    public  String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
+        return "vets/index";
     }
 }
