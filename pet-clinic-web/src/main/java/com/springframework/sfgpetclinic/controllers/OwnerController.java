caf8c8edbcbb9c2bebdabcdd93e4b45a5a9795c3
Nidhal Ferjani <nidhal.ferjani@gmail.com>
2019-06-02 16:39:43 +0100

    added Owners List on Page index and updated Controller. closes #13

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.springframework.sfgpetclinic.controllers;
 
+import com.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/", "/index", "/index.html",""})
-    public String listOwners() {
-        System.out.println("je suis dans controller Owners");
+    public String listOwners(Model model) {
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
