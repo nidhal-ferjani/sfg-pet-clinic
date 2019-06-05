27b55888146ec677c563e5fd9db69b9e3aa13eb0
Nidhal Ferjani <nidhal.ferjani@gmail.com>
2019-06-05 16:10:42 +0100

    Added Contact info Properties to Owner. closes #25

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
@@ -1,11 +1,23 @@
 package com.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Digits;
 import java.util.Set;
 
 /**
  * Created by Nidhal on 26/05/2019.
  */
 public class Owner extends Person{
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephone")
+    @Digits(fraction = 0, integer = 10)
+    private String telephone;
 
     private Set<Pet> pets;
 
@@ -15,5 +27,29 @@ public class Owner extends Person{
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
