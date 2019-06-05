1949840acea847276f1f0ac560aff2a791cd70fc
Nidhal Ferjani <nidhal.ferjani@gmail.com>
2019-06-05 14:06:32 +0100

    added Vists, updated Owner with pets relation. closes #23

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Nidhal on 26/05/2019.
  */
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,46 @@
+package com.springframework.sfgpetclinic.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import java.time.LocalDate;
+
+/**
+ * Created by Nidhal on 05/06/2019.
+ */
+public class Visit extends BaseEntity {
+
+    @Column(name = "visit_date")
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "pet_id")
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
