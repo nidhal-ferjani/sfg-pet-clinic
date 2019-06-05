8ddddd873d107bd14bba984cf84519a84201d6bb
Nidhal Ferjani <nidhal.ferjani@gmail.com>
2019-06-05 18:54:57 +0100

    added Specialities Map Service Impl. closes #28

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package com.springframework.sfgpetclinic.services;
+
+import com.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Nidhal on 05/06/2019.
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/SpecialitiesServiceMap.java
@@ -0,0 +1,40 @@
+package com.springframework.sfgpetclinic.services.map;
+
+import com.springframework.sfgpetclinic.model.Speciality;
+import com.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Nidhal on 05/06/2019.
+ */
+
+@Service
+public class SpecialitiesServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService{
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+}
